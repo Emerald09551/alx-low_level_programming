7f6c79de3dd25ba7007f474b093a58fdbfe2baef
Okwu Emperor <emperorokwu09551@gmail.com>
2022-09-22 21:23:19 -0700

    Trying it

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,45 @@
 #include "main.h"
-#include <string.h>
-
 /**
- * infinite_add - adds two numbers
- * - both numbers would be strings
- * - the result should be stored in a buffer (string)
- * @n1: first number
- * @n2: second number
- * @r: a buffer where the result should be stored
- * @size_r: the size of the buffer
- *
- * Return: the buffer holding the result
+ * infinite_add - Function that adds two numbers.
+ * @n1: First number.
+ * @n2: Second number.
+ * @r: Buffer that stores result.
+ * @size_r: Size of buffer.
+ * Return: Characeter added.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int size_n1 = 0, size_n2 = 0, op, bg, dr1, dr2, carryover = 0;
+	int i, j, k, l, m, n;
 
-	while (n1[size_n1])
-		size_n1++;
-	while (n2[size_n2])
-		size_n2++;
-	bg = size_n1 > size_n2 ? size_n1 : size_n2;
-	if (size_r <= bg + 1)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-	r[bg + 1] = '\0';
-	size_n1--, size_n2--, size_r--;
-	dr1 = n1[size_n1] - 48;
-	dr2 = n2[size_n2] - 48;
-	while (bg >= 0)
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		op = dr1 + dr2 + carryover;
-		carryover = (op >= 10) ? op / 10 : 0;
-		r[bg] = (op % 10) + 48;
-		if (size_n1 > 0)
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
 		{
-			size_n1--;
-			dr1 = n1[size_n1] - 48;
+			break;
 		}
-		else
-			dr1 = 0;
-		if (size_n2 > 0)
-		{
-			size_n2--;
-			dr2 = n2[size_n2] - 48;
-		}
-		else
-			dr2 = 0;
-		bg--, size_r--;
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-	if (*r == '0')
-		return (r + 1);
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
 	return (r);
-}
+}}
